ac1e081e167b0fdaf0ff9f439580e14f9a49fec7
Matt Ward <matt.ward@xamarin.com>
2016-06-10 15:45:15 +0100

    Add test console app.

---
+++ PackageReaderTest/NuGetPackageExtractor.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using NuGet.Common;
+using NuGet.Packaging;
+
+namespace PackageReaderTest
+{
+	public class NuGetPackageExtractor
+	{
+		PackageSaveMode packageSaveMode = PackageSaveMode.Defaultv3;
+
+		public void Extract (string fileName, string targetPath)
+		{
+			using (var stream = new FileStream (fileName, FileMode.Open, FileAccess.Read)) {
+				Extract (stream, targetPath);
+			}
+		}
+
+		public void Extract (Stream nupkgStream, string targetPath)
+		{
+			var tempHashPath = Path.Combine(targetPath, Path.GetRandomFileName());
+			var targetNuspec = Path.Combine(targetPath, Path.GetRandomFileName ());
+			var targetNupkg = Path.Combine(targetPath, Path.GetRandomFileName());
+			targetNupkg = Path.ChangeExtension (targetNupkg, ".nupkg");
+			var hashPath = Path.Combine(targetPath, Path.GetRandomFileName());
+
+			using (var packageReader = new PackageArchiveReader(nupkgStream))
+			{
+				var nuspecFile = packageReader.GetNuspecFile();
+				if ((packageSaveMode & PackageSaveMode.Nuspec) == PackageSaveMode.Nuspec)
+				{
+					packageReader.ExtractFile(nuspecFile, targetNuspec);
+				}
+
+				if ((packageSaveMode & PackageSaveMode.Files) == PackageSaveMode.Files)
+				{
+					var nupkgFileName = Path.GetFileName(targetNupkg);
+					var hashFileName = Path.GetFileName(hashPath);
+					var packageFiles = packageReader.GetFiles()
+						.Where(file => ShouldInclude(file, nupkgFileName, nuspecFile, hashFileName));
+					var packageFileExtractor = new PackageFileExtractor(
+						packageFiles,
+						XmlDocFileSaveMode.None);
+					packageReader.CopyFiles(
+						targetPath,
+						packageFiles,
+						packageFileExtractor.ExtractPackageFile,
+						CancellationToken.None);
+				}
+
+				string packageHash;
+				nupkgStream.Position = 0;
+				packageHash = Convert.ToBase64String(new CryptoHashProvider("SHA512").CalculateHash(nupkgStream));
+
+				File.WriteAllText(tempHashPath, packageHash);
+			}
+		}
+
+		private static bool ShouldInclude(
+			string fullName,
+			string nupkgFileName,
+			string nuspecFile,
+			string hashFileName)
+		{
+			// Not all the files from a zip file are needed
+			// So, files such as '.rels' and '[Content_Types].xml' are not extracted
+			var fileName = Path.GetFileName(fullName);
+			if (fileName != null)
+			{
+				if (fileName == ".rels")
+				{
+					return false;
+				}
+				if (fileName == "[Content_Types].xml")
+				{
+					return false;
+				}
+			}
+
+			var extension = Path.GetExtension(fullName);
+			if (extension == ".psmdcp")
+			{
+				return false;
+			}
+
+			if (string.Equals(fullName, nupkgFileName, StringComparison.OrdinalIgnoreCase)
+			    || string.Equals(fullName, hashFileName, StringComparison.OrdinalIgnoreCase)
+			    || string.Equals(fullName, nuspecFile, StringComparison.OrdinalIgnoreCase))
+			{
+				// Return false when the fullName is the nupkg file or the hash file.
+				// Some packages accidentally have the nupkg file or the nupkg hash file in the package.
+				// We filter them out during package extraction
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
+
---
+++ PackageReaderTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace PackageReaderTest
+{
+	class Program
+	{
+		string fileName;
+
+		public static void Main (string[] args)
+		{
+			try {
+				var program = new Program ();
+				program.Run (new [] { "Xamarin.Android.Support.v4.23.1.1.nupkg" });
+			} catch (Exception ex) {
+				Console.WriteLine (ex.ToString ());
+			}
+		}
+
+		void Run (string[] args)
+		{
+			if (args.Length == 0) {
+				ShowUsage ();
+				return;
+			}
+
+			fileName = Path.GetFullPath (args[0]);
+
+			string targetPath = Path.GetDirectoryName (typeof (Program).Assembly.Location);
+			targetPath = Path.Combine (targetPath, "extract");
+			if (!Directory.Exists (targetPath)) {
+				Directory.CreateDirectory (targetPath);
+			}
+
+			var extractor = new NuGetPackageExtractor ();
+			extractor.Extract (fileName, targetPath);
+		}
+
+		void ShowUsage ()
+		{
+			Console.WriteLine ("PackageReaderTest NuGetPackageFileName.nupkg");
+		}
+	}
+}
